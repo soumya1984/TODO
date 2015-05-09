85ef5dd874786d4d2d4e96f8af435a7e6f2ba93a
souachar <souachar@localhost>
2015-05-09 11:51:41 -0700

    commit

--- src/main/java/com/app/todo/mongodb/MongoDBConnectionHelper.java
+++ src/main/java/com/app/todo/mongodb/MongoDBConnectionHelper.java
@@ -10,7 +10,7 @@ public class MongoDBConnectionHelper {
 	private static String DB_URL = "localhost";
 	private static String DB_NAME = "sensitouch";
 	private final static int MONGO_DB_PORT_NO = 27017;
-	private static DBCollection conn;
+	private static DBCollection conn=null;
 
 	private static DB getConnection() throws UnknownHostException {
 		MongoClient mongodb = new MongoClient(DB_URL, MONGO_DB_PORT_NO);
@@ -20,8 +20,6 @@ public class MongoDBConnectionHelper {
 
 	public static DBCollection getCollection(String collectionName)
 			throws UnknownHostException {
-		if (conn != null)
-			return conn;
 		return conn = getConnection().getCollection(collectionName);
 
 	}
